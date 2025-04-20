49ecb453ab5723d1e1d53cc6a156b13dffee2820
Jeddy-Xie <ybz3vj@virginia.edu>
2025-04-20 16:41:09 -0400

    feat: enhance metric computation - Add support for seq2seq metrics - Improve metric calculation accuracy - Add new evaluation metrics

--- utils/compute_metric.py
+++ utils/compute_metric.py
@@ -5,14 +5,19 @@ def compute_metrics(y_true, y_pred):
     mse = mean_squared_error(y_true, y_pred)
     mae = mean_absolute_error(y_true, y_pred)
     return mse, mae
-
-
 def append_score(scores, new_score):
     for i, score in enumerate(scores):
         if score['model'] == new_score['model'] and score['h-step Forecast'] == new_score['h-step Forecast']:
             scores[i] = new_score
             return
     scores.append(new_score)
+
+def append_model(models, new_model):
+    for i, model in enumerate(models):
+        if model['model'] == new_model['model'] and model['h-step Forecast'] == new_model['h-step Forecast']:
+            models[i] = new_model
+            return
+    models.append(new_model)
 
 
 def compute_metrics_seq2seq(y_true, y_pred):
